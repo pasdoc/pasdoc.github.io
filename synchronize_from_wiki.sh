8291bebb1a478d173ce138297c4915409a632925
Michalis Kamburelis <michalis.kambi@gmail.com>
2021-01-25 04:20:06 +0100

    Rename main file to index, to be automatically used as default

--- synchronize_from_wiki.sh
+++ synchronize_from_wiki.sh
@@ -3,10 +3,15 @@ set -eu
 
 rm -Rf src/*.asciidoc
 cp -f ../pasdoc.wiki/*.asciidoc src/
+
+# TODO: we should "mv" to index name, not copy, once we're sure we have no remaining links to Home
+cp src/Home.asciidoc src/index.asciidoc
+
 for F in src/*.asciidoc; do
   # Increase header indentation for all files
   sed --in-place -e 's|^#### |##### |' "$F"
   sed --in-place -e 's|^### |#### |' "$F"
   sed --in-place -e 's|^## |### |' "$F"
   sed --in-place -e 's|^# |## |' "$F"
+  sed --in-place -e 's|link:Home\[PasDoc\]|link:index[PasDoc]|' "$F"
 done
