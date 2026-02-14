3d3435bd6362847ef9e3059e4da360f77fdbd57a
Michalis Kamburelis <michalis.kambi@gmail.com>
2026-02-14 06:22:44 +0100

    New autodoc

--- docs/autodoc/html/tipuesearch/tipuesearch.js
+++ docs/autodoc/html/tipuesearch/tipuesearch.js
@@ -21,7 +21,7 @@ http://www.tipue.com/search
           'footerPages'            : 3,
           'highlightTerms'         : true,
           'imageZoom'              : true,
-          'minimumLength'          : 3,
+          'minimumLength'          : 1,
           'newWindow'              : false,
           'show'                   : 10,
           'showContext'            : true,
